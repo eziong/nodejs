233203f4fcc71935ea10e7b4150db8a6a66c0f84
eziong <bsjwoong@gmail.com>
2021-01-12 20:03:48 +0900

    2021/01/12

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var User = require("./models/User");
 
 var app = express();
 
@@ -16,10 +19,24 @@ mongoose
     .then(() => console.log("MongoDB is connected"))
     .catch((err) => console.log(err));
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 app.set("port", 3000);
 
 app.get("/", (req, res) => {
     res.send("hello world");
+});
+
+app.post("/register", (req, res) => {
+    var user = new User(req.body);
+
+    user.save((err, docs) => {
+        if (err) return res.json({ success: false, err });
+        else return res.status(200).json({ success: true, docs });
+    });
 });
 
 app.listen(app.get("port"), () => {
